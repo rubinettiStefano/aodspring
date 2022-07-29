62a67814fc4dc4b09b4c110efebf0510ce2c9fc6
rubinettiStefano <stefano.rubinetti@gmail.com>
2022-07-29 12:49:37 +0200

    First Commit

---
+++ AoDSpring/src/main/java/com/generation/aod/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.generation.aod;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AoDSpringApplication.class);
+	}
+
+}
---
+++ AoDSpring/src/test/java/com/generation/aod/AoDSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.aod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AoDSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
